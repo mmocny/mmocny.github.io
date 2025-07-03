b5f19b81dd07abdd464ed241956c6f8ae04454e6
Michal Mocny <mmocny@gmail.com>
2025-07-03 08:42:35 -0400

    Update LCP size to be in pixels not bytes

--- snippets/SoftNavs.js
+++ snippets/SoftNavs.js
@@ -49,7 +49,7 @@ function logMetric(metric) {
   if (metric.name.endsWith("LCP")) {
     const lcpEntry = metric.entries[0];
     logArgs.push(
-      `${(lcpEntry.size / 1024).toFixed(1)}KB`,
+      `${(lcpEntry.size / 1000).toFixed(1)} kpx`,
       lcpEntry.element || `URL: ${lcpEntry.url}`
     );
   } else if (metric.attribution.pageUrl) {
